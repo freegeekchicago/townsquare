647f196068c4784a84a565aad3c3628a2dfd0577
David Eads <davideads@gmail.com>
2011-03-25 18:24:18 -0500

    Added townsquare_volunteer.js, our current bucket for behaviors.

---
+++ modules/townsquare_volunteer/js/townsquare_volunteer.js
@@ -0,0 +1,8 @@
+(function($) {
+Drupal.behaviors.volunteerGrowTextarea = {
+  attach: function(context) {
+    $('.field-name-field-session-notes textarea', context).elastic();
+    $('.field-name-field-event-description textarea', context).elastic();
+  }
+};
+})(jQuery);
