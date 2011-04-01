6ac6836695f62ff00d867c5deebcb4066a6d14ac
David Eads <davideads@gmail.com>
2011-03-31 21:24:23 -0500

    Updated login block to include links instead of form for anonymous user, and markup surrounding login block.

--- modules/townsquare_core/townsquare-login.tpl.php
+++ modules/townsquare_core/townsquare-login.tpl.php
@@ -1,22 +1,29 @@
 <?php if (!$user->uid): ?>
   <?php 
     $form = drupal_get_form('user_login_block');
-    $form['links']['#markup'] = l(t('Register for a new account'), 'user/register');
+    $form['links'] = array(
+      '#markup' => l(t('Register'), 'user/register'),
+      '#weight' => 500,
+      '#prefix' => '<div class="links">',
+      '#suffix' => '</div>',
+    );
     print render($form);
   ?>
 <?php else: ?>
-  <div id="login-meta">
-    <div class="login-username">
-      <?php print t(
-        'Logged in as !user', 
-        array('!user' => theme('username', array('account' => $user)))
-      );
-      ?>
+  <div class="login-wrapper">
+    <div id="login-meta">
+      <div class="login-username">
+        <?php print t(
+          'Logged in as !user', 
+          array('!user' => theme('username', array('account' => $user)))
+        );
+        ?>
+      </div>
+      <ul class="links">
+        <li class="settings"><?php print l(t('Account settings'), 'user/'. $user->uid .'/edit'); ?></li>
+        <li class="profile"><?php print l(t('Volunteer profile'), 'user/'. $user->uid); ?></li>
+      </ul>
     </div>
-    <ul class="links">
-      <li class="settings"><?php print l(t('Account settings'), 'user/'. $user->uid .'/edit'); ?></li>
-      <li class="profile"><?php print l(t('Volunteer profile'), 'user/'. $user->uid); ?></li>
-    </ul>
+    <?php print theme('user_picture', array('account' => $user)); ?>
   </div>
-  <?php print theme('user_picture', array('account' => $user)); ?>
 <?php endif; ?>
