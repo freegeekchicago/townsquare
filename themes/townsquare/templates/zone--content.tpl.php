e39c6785a4ec47b9dcbbbf84b7856be72e99286f
David Eads <davideads@gmail.com>
2012-01-06 19:06:36 -0600

    Drop grid-12 class from messages variable in tpl.

--- themes/townsquare/templates/zone--content.tpl.php
+++ themes/townsquare/templates/zone--content.tpl.php
@@ -1,7 +1,7 @@
 <?php if ($wrapper): ?><div<?php print $attributes; ?>><?php endif; ?>  
   <div<?php print $content_attributes; ?>>    
     <?php if ($messages): ?>
-      <div id="messages" class="grid-<?php print $columns; ?>"><?php print $messages; ?></div>
+      <div id="messages"><?php print $messages; ?></div>
     <?php endif; ?>
     <?php print $content; ?>
   </div>
