74e4838cc42fed6bfdbc10aee61fd06435c61dc3
David Eads <davideads@gmail.com>
2012-02-09 00:05:12 -0600

    Drop jquery elastic plugin.

--- modules/townsquare/townsquare_volunteer/js/townsquare_volunteer-bulk-edit.js
+++ modules/townsquare/townsquare_volunteer/js/townsquare_volunteer-bulk-edit.js
@@ -1,11 +1,5 @@
 (function($) {
 
-Drupal.behaviors.volunteerGrowTextarea = {
-  attach: function(context) {
-    $('.field-name-field-session-notes textarea', context).elastic();
-    $('.field-name-field-event-description textarea', context).elastic();
-  }
-};
 Drupal.behaviors.volunteerExtendPage = {
   attach: function(context) {
     var adjusted = $('#volunteer-sessions').data('heightAdjusted');
