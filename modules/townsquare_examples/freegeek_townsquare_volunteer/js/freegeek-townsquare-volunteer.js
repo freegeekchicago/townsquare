897a6c1a0b24507ddd3d96010e38da825e73e05d
David Eads <davideads@gmail.com>
2012-06-02 17:07:38 -0500

    focus username field on signup form load; fixes #158

--- modules/townsquare_examples/freegeek_townsquare_volunteer/js/freegeek-townsquare-volunteer.js
+++ modules/townsquare_examples/freegeek_townsquare_volunteer/js/freegeek-townsquare-volunteer.js
@@ -1,7 +1,15 @@
 (function($) {
+
 Drupal.behaviors.hideMessages = {
   attach: function(context) {
     setInterval(function() { $('.messages.status').slideUp('1200'); }, 6000);
   }
 };
+
+Drupal.behaviors.focusName = {
+  attach: function(context) {
+    $('#edit-name').focus();
+  }
+};
+
 })(jQuery);
