8e44367ea8164cae574d9bd30d4ae978a5ffcf1f
David Eads <davideads@gmail.com>
2012-06-11 23:01:40 -0500

    Remove freegeek theme to create subtheme

--- themes/freegeek/templates/region--primary-tasks.tpl.php
+++
@@ -1,8 +0,0 @@
-<div<?php print $attributes; ?>>
-  <div<?php print $content_attributes; ?>>
-    <?php if ($primary_tasks): ?>
-      <div id="primary_tasks"><?php print drupal_render($primary_tasks); ?></div>
-    <?php endif; ?>    
-    <?php print $content; ?>
-  </div>
-</div>
--- themes/freegeek/templates/zone--content.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php if ($wrapper): ?><div<?php print $attributes; ?>><?php endif; ?>  
-  <div<?php print $content_attributes; ?>>    
-    <?php if ($messages): ?>
-      <div id="messages"><?php print $messages; ?></div>
-    <?php endif; ?>
-    <?php print $content; ?>
-  </div>
-<?php if ($wrapper): ?></div><?php endif; ?>
