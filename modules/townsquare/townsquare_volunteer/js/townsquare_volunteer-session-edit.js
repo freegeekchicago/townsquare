a71a000c385d013d321846cef870037ff43838ca
Sabine Ye <wasabine@gmail.com>
2012-07-04 11:19:42 -0500

    Issue #126 about the no signout button is fixed

--- modules/townsquare/townsquare_volunteer/js/townsquare_volunteer-session-edit.js
+++ modules/townsquare/townsquare_volunteer/js/townsquare_volunteer-session-edit.js
@@ -36,15 +36,30 @@ Drupal.behaviors.toggleFields = {
       var form = $(this).parents('form');
       var duration = $('input[name="field_session_duration[und][0][value]"]', form);
       var hours = $('.form-item-field-session-hours-und-0-value input, .form-item-field-session-hours-und-0-value2 input', form);
+      var noSignout = $('.form-item-field-session-no-signout-und input', form)
       if ($(this).is(':checked')) {
         duration.removeAttr('readonly');
         hours.attr('readonly', true);
+        noSignout.attr('readonly',true);
       }
       else {
         duration.attr('readonly', true);
         hours.removeAttr('readonly');
       }
     });
+    $('.form-item-field-session-no-signout-und input').not(':disabled').each(function() {
+      var form = $(this).parents('form');
+      var duration = $('input[name="field_session_duration[und][0][value]"]', form);
+      var hours = $('.form-item-field-session-hours-und-0-value input, .form-item-field-session-hours-und-0-value2 input', form);
+      var setDuration=$('.form-item-field-session-override-duration-und input',form);
+      if ($(this).is(':checked')) {
+        duration.removeAttr('readonly');
+        hours.attr('readonly', true);
+        setDuration('readonly',true);
+      }
+      
+    });
+
   }
 };
 
